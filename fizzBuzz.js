5bd51229ab6bb04da90d9343798102d0d26669e2
MaSsoumeh <47733451+MaSsoumeh@users.noreply.github.com>
2021-08-26 07:24:52 +0430

    Add files via upload
    The first JavaScript code for fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,25 @@
+﻿// Divisable by 3 => Fizz
+// Divisable by 5 => Buzz
+// Divisable by 3 and 5 => FizzBuzz
+// Not devisable by 3 or 5 => input
+// Not a number => "Not a number"
+'use strict'
+
+let input = +prompt("Enter a Number:", 1);
+console.log(fizzBuzz(input));
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number') 
+        return "Not a number";
+    
+    if ((input % 3 === 0) && (input % 5 === 0))
+        return "FizzBuzz";
+    
+    if (input % 3 === 0) 
+        return "Fizz";
+
+    if (input % 5 == 0)
+        return "Buzz";
+
+    return input;
+}
